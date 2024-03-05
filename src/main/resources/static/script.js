81909f92b132d92af29b8ffd6bb8bca936928c38
Yash Chouhan <108524150+YashChouhan1@users.noreply.github.com>
2024-03-05 12:33:34 +0530

    Update script.js

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -4,7 +4,7 @@ const dataContainer = document.getElementById('dataContainer');
 fetchButton.addEventListener('click', fetchData);
 
 function fetchData() {
-    fetch('http://backend:8080/all')
+    fetch('http://backend:9000/all')
         .then(response => response.json())
         .then(data => {
             displayData(data);
